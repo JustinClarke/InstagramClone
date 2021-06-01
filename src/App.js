4263be117439847d811fc0b590d1bc8de62ba7cb
Justin Clarke <justinclarke241@gmail.com>
2021-06-01 17:52:46 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -154,12 +154,12 @@ function App() {
       
       </Modal>
       <div className="app__header">
-        <img
+        {/* <img
           className="app__headerimage" 
           src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"
           alt=""
-        />
-        {/* <h1>FireGram</h1> */}
+        /> */}
+        <h1>Instagram</h1>
         {user ? (
             <Button onClick ={()=>auth.signOut()}>Logout</Button>
             ): (
