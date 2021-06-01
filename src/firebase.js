f68be3b65411259c34e24863a069b03d6274446a
Justin Clarke <justinclarke241@gmail.com>
2021-06-01 17:43:40 +0530

    final comit

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,17 @@
-import firebase from "firebase";
+import firebase from 'firebase';
 
-const firebaseApp = firebase.initalizeApp({
-    apiKey: "AIzaSyBpvl4fs8DYv2xa1uwT039V_6Anva6mBh8",
-    authDomain: "crud-post-app.firebaseapp.com",
-    projectId: "crud-post-app",
-    storageBucket: "crud-post-app.appspot.com",
-    messagingSenderId: "503422705615",
-    appId: "1:503422705615:web:6135e58433ab80fbfafcba",
-    measurementId: "G-HH7TJ0F6F3"
-    });
+const FirebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyBpvl4fs8DYv2xa1uwT039V_6Anva6mBh8",
+  authDomain: "crud-post-app.firebaseapp.com",
+  projectId: "crud-post-app",
+  storageBucket: "crud-post-app.appspot.com",
+  messagingSenderId: "503422705615",
+  appId: "1:503422705615:web:6135e58433ab80fbfafcba",
+  measurementId: "G-HH7TJ0F6F3"
+});
 
-  const db = firebaseApp.firestore();
-  const auth = firebase.auth();
-  const storage = firebase.storage();
+const db = FirebaseApp.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
 
-  export { db, auth, storage };
+export { db, auth, storage };
